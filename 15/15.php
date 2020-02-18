8bd4d6ed2e853e2c04c0a7576137a392456d9dd2
SavchukDev <savchuk421@gmail.com>
2020-02-18 20:11:20 +0200

    upd

--- 15/15.php
+++ 15/15.php
@@ -1,24 +1,29 @@
 <?php
 //15. Написать калькулятор. Переменная $a = изменяемое число. Переменная $b = изменяемое число. Переменная $operator = ‘+’ или ‘-’ или ‘/’ или ‘*’ или '%' (остаток от деления). На экран выводить результат в зависимости от значений этих переменных. Не забудьте про деление на 0, если надо - выдавать ошибку что на 0 делить нельзя.
+include "form.html";
+$firstNum = $_POST['firstNum'];
+$secondNum = $_POST['secondNum'];
+$operator = $_POST['operator'];
 
-$a = $_GET['a'];
-$b = $_GET['b'];
-$operator = $_GET['operator'];
+if ($operator == '/' && $secondNum == 0) {
+    echo "Test";
 
-if ($operator == '*') {
-    $result = $a * $b;
+} elseif ($operator == '*') {
+    $result = $firstNum * $secondNum;
+} elseif ($operator == '/') {
+    $result = $firstNum / $secondNum;
+} elseif ($operator == '+') {
+    $result = $firstNum + $secondNum;
+} elseif ($operator == '-') {
+    $result = $firstNum - $secondNum;
 }
-if ($operator = '/') {
-    $result = $a / $b;
-}
-if ($operator = '+') {
-    $result = $a + $b;
-}
-if ($operator = '-') {
-    $result = $a - $b;
-}
-echo
-else if ($operator = '/' && $b = 0) {
-    echo 'Обратно в школу!';
-}
+
+echo $result;
+
+
+
+
+
+
+
 
